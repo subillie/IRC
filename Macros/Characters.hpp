e8a4c5afc3ebcc1c227cf21cb8d55bd2b633f6e1
hyobicho <hyobicho@student.42seoul.kr>
2024-01-26 23:40:25 +0900

    chore: 문자 형식 매크로
    Co-authored-by: subillie <ddubi701@gmail.com>
    Co-authored-by: inwoo0115 <wonjilee@student.42seoul.kr>

---
+++ Macros/Characters.hpp
@@ -0,0 +1,12 @@
+#ifndef CHARACTERS_HPP
+#define CHARACTERS_HPP
+
+#include <string>
+
+const std::string LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+const std::string UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const std::string SPECIAL_CHAR = "[]\\`_-^{|}";
+const std::string DIGIT = "0123456789";
+const std::string CRLF = "\r\n";
+
+#endif
