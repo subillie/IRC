3f243be4b25dc30a758b05a65ebc75c158cbed60
hyobicho <hyobicho@student.42seoul.kr>
2024-01-26 23:38:40 +0900

    feat: messenger class 구현 중
    Co-authored-by: subillie <ddubi701@gmail.com>
    Co-authored-by: inwoo0115 <wonjilee@student.42seoul.kr>

---
+++ RequestHandler/ Messenger/Messenger.hpp
@@ -0,0 +1,21 @@
+#ifndef MESSENGER_HPP
+#define MESSENGER_HPP
+
+#include <string>
+#include <vector>
+
+class Messenger {
+ private:
+  std::string _prefix;
+  std::string _command;
+  std::string _trailing;
+
+ public:
+  Messenger();
+  void setPrefix(const std::string& prefix);
+  void setCommand(const std::string& command);  // token[1] + token[2] ....
+  void setTrailing(const std::string& trailing);
+  void sendToClient(int fd);
+};
+
+#endif
