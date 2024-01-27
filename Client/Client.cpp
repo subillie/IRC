336398684fb97bb605f1ec1a25f5d07b1f338ad8
Hyobin Cho <hyobicho@student.42seoul.kr>
2024-01-27 22:13:49 +0900

    :feat client hostname 추가

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -9,6 +9,8 @@ void Client::setNickname(const std::string &nickname) { _nickname = nickname; }
 
 void Client::setPassword(const std::string &password) { _password = password; }
 
+void Client::setHostname(const std::string &hostname) { _hostname = hostname; }
+
 const int &Client::getFd() const { return _fd; }
 
 const std::string &Client::getNickname() const { return _nickname; }
@@ -16,6 +18,8 @@ const std::string &Client::getNickname() const { return _nickname; }
 const std::string &Client::getPassword() const { return _password; }
 
 const std::string &Client::getUsername() const { return _username; }
+
+const std::string &Client::getHostname() const { return _hostname; }
 
 bool Client::getIsRegistered() const { return _isRegistered; }
 
