753732d466d5f15e432d5164486c7e03588c63f6
Hyobin Cho <hyobicho@student.42seoul.kr>
2024-01-27 22:13:28 +0900

    feat: Messenger class 구현

--- RequestHandler/Messenger/Messenger.cpp
+++ RequestHandler/Messenger/Messenger.cpp
@@ -1,14 +1,96 @@
 #include "Messenger.hpp"
 
-Messenger::Messenger() {}
+Messenger::Messenger() : _prefix(""), _param(""), _trailing("") {}
+
+#include "../../Print/Print.hpp"
+
+// Error function
+void Messenger::ErrNeedMoreParams(int fd) {
+  printRed("NeedMoreParams");
+  sendToClient(fd);
+}
+
+void Messenger::ErrAlreadyRegistered(int fd) {
+  printRed("AlreadyRegistered");
+  sendToClient(fd);
+}
+
+void Messenger::ErrPasswdMismatch(int fd) {
+  printRed("ErrPasswdMismatch");
+  sendToClient(fd);
+}
+
+void Messenger::ErrNoNickNameGiven(int fd) {
+  printRed("ErrNoNickNameGiven");
+  sendToClient(fd);
+}
+
+void Messenger::ErrErroneusNickName(int fd) {
+  printRed("ErrErroneusNickName");
+  sendToClient(fd);
+}
+
+void Messenger::ErrNickNameInUse(int fd) {
+  printRed("ErrNickNameInUse");
+  sendToClient(fd);
+}
+
+void Messenger::ErrNoOrigin(int fd) {
+  printRed("ErrNoOrigin");
+  sendToClient(fd);
+}
+
+void Messenger::ErrUnknownCommand(int fd) {
+  printRed("UnknownCommand");
+  sendToClient(fd);
+}
+
+void Messenger::ErrUnexpected(int fd) {
+  printRed("Unexpected");
+  sendToClient(fd);
+}
+
+// Reply function
+void Messenger::RplWelcome(int fd) {
+  Client* client = Server::_clientFds[fd];
+  setPrefix(":" + SERVER);
+  const std::string& nick = client->getNickname();
+  const std::string& username = client->getUsername();
+  const std::string& hostname = client->getHostname();
+  setParam(RPL_WELCOME + " " + nick);
+  setTrailing(":Welcome to private irc server! " + nick + "!" + username + "@" +
+              hostname);
+  printRed("RplWelcome");
+  sendToClient(fd);
+}  // 001
+
+void Messenger::RplYourHost(int fd) {
+  printRed("RplYourHost");
+  sendToClient(fd);
+}  // 002
+
+void Messenger::RplCreated(int fd) {
+  printRed("RplCreated");
+  sendToClient(fd);
+}  // 003
+
+void Messenger::RplMyinfo(int fd) {
+  printRed("RplMyinfo");
+  sendToClient(fd);
+}  // 004
 
 void Messenger::setPrefix(const std::string& prefix) { _prefix = prefix; }
 
-void Messenger::setCommand(const std::string& command) { _command = command; }
+void Messenger::setParam(const std::string& param) { _param = param; }
 
-void Messenger::sendToClient(int fd, const std::string& msg) {
-  const std::string reply = msg + CRLF;
-  if (send(fd, &reply, reply.length(), 0) == -1) {
+void Messenger::setTrailing(const std::string& trailing) {
+  _trailing = trailing;
+}
+
+void Messenger::sendToClient(int fd) {
+  std::string response = _prefix + " " + _param + " " + _trailing + CRLF;
+  printCyan(response);
+  if (send(fd, response.c_str(), response.length(), 0) == -1) {
     throw std::runtime_error("Send error");
   }
 }
--- RequestHandler/Messenger/Messenger.hpp
+++ RequestHandler/Messenger/Messenger.hpp
@@ -1,21 +1,43 @@
 #ifndef MESSENGER_HPP
 #define MESSENGER_HPP
 
+#include <sys/socket.h>
+
 #include <string>
 #include <vector>
 
+#include "../../Client/Client.hpp"
 #include "../../Macros/Characters.hpp"
+#include "../../Server/Server.hpp"
+#include "ErrorMacros.hpp"
+#include "Replies.hpp"
 
 class Messenger {
  private:
   std::string _prefix;
-  std::string _command;
+  std::string _param;
   std::string _trailing;
 
  public:
   Messenger();
+  // Error function
+  void ErrNeedMoreParams(int fd);
+  void ErrAlreadyRegistered(int fd);
+  void ErrPasswdMismatch(int fd);
+  void ErrNoNickNameGiven(int fd);
+  void ErrErroneusNickName(int fd);
+  void ErrNickNameInUse(int fd);
+  void ErrNoOrigin(int fd);
+  void ErrUnknownCommand(int fd);
+  void ErrUnexpected(int fd);  // 에러코드가 정해져 있지 않은 에러
+  // Reply function
+  void RplWelcome(int fd);   // 001
+  void RplYourHost(int fd);  // 002
+  void RplCreated(int fd);   // 003
+  void RplMyinfo(int fd);    // 004
+  //  make response
   void setPrefix(const std::string& prefix);
-  void setCommand(const std::string& command);  // token[1] + token[2] ....
+  void setParam(const std::string& param);
   void setTrailing(const std::string& trailing);
   void sendToClient(int fd);
 };
