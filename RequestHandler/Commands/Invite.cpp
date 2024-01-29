2e5c0ab774662cea1dfd58d6713ef2ca60bddf33
Subin Lee <ddubi701@gmail.com>
2024-01-29 19:30:01 +0900

    feat: start implementing invite

--- RequestHandler/Commands/Invite.cpp
+++ RequestHandler/Commands/Invite.cpp
@@ -1,3 +1,11 @@
 #include "../RequestHandler.hpp"
 
-void RequestHandler::invite() {}
+// /invite <nickname> <channel>
+void RequestHandler::invite() {
+  // 인자가 2개가 아닐 때
+  if (_token.size() < 3) {
+    _msg.ErrNeedMoreParams(_fd, _token[0]);
+    return;
+  }
+  // 채널이 존재하지 않을 때
+}
