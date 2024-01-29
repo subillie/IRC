d897747b491eaccfde08f76176e00a4649785a30
Subin Lee <ddubi701@gmail.com>
2024-01-29 20:35:24 +0900

    feat: error handling 중

--- RequestHandler/Commands/Invite.cpp
+++ RequestHandler/Commands/Invite.cpp
@@ -7,5 +7,31 @@ void RequestHandler::invite() {
     _msg.ErrNeedMoreParams(_fd, _token[0]);
     return;
   }
-  // 채널이 존재하지 않을 때
+  std::string nickname = _token[1];
+  std::string channel = _token[2];
+
+  // 채널이 존재하지 않거나 채널에 유저가 한 명도 없을 때
+  if (Server::_channelNames.find(channel) == Server::_channelNames.end() ||
+      Server::_channelNames[channel]->getMembers().empty()) {
+    _msg.ErrNoSuchChannel(_fd, channel);
+    return;
+  }
+
+  // 초대하려는 유저가 채널의 멤버가 아닐 때
+  Channel *chanToInvite = Server::_channelNames[channel];
+  std::list<std::string> membList = chanToInvite->getMembers();
+  if (std::find(membList.begin(), membList.end(), nickname) == membList.end()) {
+    _msg.ErrNotOnChannel(_fd, channel);
+    return;
+  }
+
+  // 채널이 Invite Only이고 유저가 채널의 Operator가 아닐 때
+  std::list<char> modeList = chanToInvite->getModes();
+  std::list<std::string> operList = chanToInvite->getOperators();
+  if (std::find(modeList.begin(), modeList.end(), INVITE_ONLY) !=
+          modeList.end() &&
+      std::find(operList.begin(), operList.end(), nickname) == operList.end()) {
+    _msg.ErrChanOPrivsNeeded(_fd, channel);
+    return;
+  }
 }
