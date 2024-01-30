877ba79bd0d6608e8aeba0e05bbf8b2324f7ca88
Subin Lee <ddubi701@gmail.com>
2024-01-30 22:15:16 +0900

    feat: 참여 중인 모든 채널의 모든 멤버에게 quit 메시지 전송

--- RequestHandler/Commands/Quit.cpp
+++ RequestHandler/Commands/Quit.cpp
@@ -14,9 +14,33 @@ void RequestHandler::quit() {
     }
   }
 
-  _msg.setParam("QUIT");
-  _msg.setTrailing(reason);
-  _msg.Error(_fd, reason);
+  // client가 참여 중인 모든 채널에 대해
+  std::string nickname = _client->getNickname();
+  std::string prefix = _client->getPrefix();
+  std::set<std::string> channels = _client->getChannels();
+  std::set<std::string>::iterator chanIter = channels.begin();
+  for (; chanIter != channels.end(); chanIter++) {
+    Channel* channel = Server::_channelNames[*chanIter];
+
+    // 채널에 있는 모든 멤버들에게 메시지 전송
+    std::set<std::string> members = channel->getMembers();
+    std::set<std::string>::iterator membIter = members.begin();
+    for (; membIter != members.end(); membIter++) {
+      int fd = Server::_clientNicks[*membIter]->getFd();
+      _msg.setPrefix(prefix + "QUIT");
+      _msg.setParam("Quit");
+      _msg.setTrailing(reason);
+      _msg.sendToClient(fd);
+    }
+    channel->removeMember(nickname);
+    channel->removeOp(nickname);
+    channel->removeInvitee(nickname);
+  }
+
+  _msg.setParam("ERROR");
+  _msg.setTrailing("Closing link: (" + Server::_clientFds[_fd]->getHostname() +
+                   ") [Quit: " + reason + "]");
+  _msg.sendToClient(_fd);
 
   Server::_clientFds.erase(_fd);
   if (close(_fd) < 0) {
--- RequestHandler/Messenger/Errors.cpp
+++ RequestHandler/Messenger/Errors.cpp
@@ -244,11 +244,3 @@ void Messenger::ErrUnexpected(int fd) {
   printRed("Unexpected");
   sendToClient(fd);
 }
-
-void Messenger::Error(int fd, const std::string& reason) {
-  _param = ERROR;
-  _trailing = "Closing link: (" + Server::_clientFds[fd]->getHostname() +
-              ") [Quit: " + reason + "]";
-  printRed("Error");
-  sendToClient(fd);
-}
