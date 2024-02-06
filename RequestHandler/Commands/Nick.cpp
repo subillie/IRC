88ed72de6e49ac059ed1380ce09e5b9d0a893309
Hyobin Cho <hyobicho@student.42seoul.kr>
2024-02-06 19:29:00 +0900

    style: 변수 명 수정

--- RequestHandler/Commands/Nick.cpp
+++ RequestHandler/Commands/Nick.cpp
@@ -9,39 +9,43 @@ void RequestHandler::nick() {
     throw("Quit");
     return;
   }
-  if (_token.size() != 2) {
+  // 인자가 없을 때
+  if (_token.size() != 2 || _token[1].empty()) {
     _msg.ErrNoNickNameGiven(_fd);
     return;
-  } else if (isExistingClient(_token[1])) {
-    _msg.ErrNickNameInUse(_fd, _token[1]);
-    return;
-  } else {
-    if (std::isdigit(_token[1][0])) {
-      _msg.ErrErroneusNickName(_fd, _token[1]);
-      return;
-    }
-    size_t pos = _token[1].find_first_not_of(
-        LOWERCASE + UPPERCASE + SPECIAL_CHAR + DIGIT, 1);
-    // 닉네임이 규칙에 맞지 않는 경우
-    if (_token[1].length() > 31 || pos != std::string::npos) {
-      _msg.ErrErroneusNickName(_fd, _token[1]);
-      return;
-    }
   }
+  const std::string& newNick = _token[1];
+  const std::string& oldNick = _client->getNickname();
+  if (isExistingClient(newNick)) {
+    _msg.ErrNickNameInUse(_fd, newNick);
+    return;
+  }
+  if (std::isdigit(newNick[0])) {
+    _msg.ErrErroneusNickName(_fd, newNick);
+    return;
+  }
+  size_t pos = newNick.find_first_not_of(
+      LOWERCASE + UPPERCASE + SPECIAL_CHAR + DIGIT, 1);
+  // 닉네임이 규칙에 맞지 않는 경우
+  if (newNick.length() > 31 || pos != std::string::npos) {
+    _msg.ErrErroneusNickName(_fd, newNick);
+    return;
+  }
+
   // nickname 설정이 되어 있을 경우
-  if (_client->getNickname() != "") {
-    Server::_clientNicks.erase(_client->getNickname());
-    Server::_clientNicks[_token[1]] = _client;
+  if (!oldNick.empty()) {
+    Server::_clientNicks.erase(oldNick);
+    Server::_clientNicks[newNick] = _client;
     // : two!root@127.0.0.1 NICK :new
     _msg.setPrefix(_client->getPrefix());
     _msg.setParam("NICK");
-    _msg.setTrailing(_token[1]);
+    _msg.setTrailing(newNick);
     // 닉네임 변경 시 모든 클라이언트에 메시지 보냄
     Server::sendToAllClients(_msg);
-    _client->setNickname(_token[1]);
+    _client->setNickname(newNick);
   } else {
-    _client->setNickname(_token[1]);
-    Server::_clientNicks[_token[1]] = _client;
+    _client->setNickname(newNick);
+    Server::_clientNicks[newNick] = _client;
   }
 }
 
