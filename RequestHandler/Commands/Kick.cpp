59bb43ed32d6214616afd5384319bc2d298c24b9
Hyobin Cho <hyobicho@student.42seoul.kr>
2024-02-10 21:57:46 +0900

    fix: Kick leaveChannel 호출하는 클라이언트 수정
    Co-authored-by: Subin Lee <subillie@users.noreply.github.com>

--- RequestHandler/Commands/Kick.cpp
+++ RequestHandler/Commands/Kick.cpp
@@ -9,27 +9,29 @@ void RequestHandler::kick() {
     return;
   }
 
+  const std::string& channelName = _token[1];
   // 채널 형식이 올바르지 않을 때
-  if (_token[1][0] != '#') {
+  if (channelName[0] != '#') {
     _msg.ErrBadChanMask(_fd);
     return;
   }
 
   // 해당 채널이 존재하지 않을 때
-  if (Server::_channelNames.find(_token[1]) == Server::_channelNames.end()) {
-    _msg.ErrNoSuchChannel(_fd, _token[1]);
+  if (Server::_channelNames.find(channelName) == Server::_channelNames.end()) {
+    _msg.ErrNoSuchChannel(_fd, channelName);
     return;
   }
 
-  Channel *chanToKick = Server::_channelNames[_token[1]];
+  Channel* chanToKick = Server::_channelNames[channelName];
+  const std::string& nick = _client->getNickname();
   std::set<std::string> memberList = chanToKick->getMembers();
   std::set<std::string> opList = chanToKick->getOps();
   // Kick을 요청한 사용자가 해당 채널에 없을 때
-  if (memberList.find(_client->getNickname()) == memberList.end()) {
-    _msg.ErrNotOnChannel(_fd, _client->getNickname());
+  if (!chanToKick->isMember(nick)) {
+    _msg.ErrNotOnChannel(_fd, nick);
     return;
     // Kick을 요청한 사용자가 권한이 없을 때
-  } else if (opList.find(_client->getNickname()) == opList.end()) {
+  } else if (!chanToKick->isOp(nick)) {
     _msg.ErrChanOPrivsNeeded(_fd, chanToKick->getName());
     return;
   }
@@ -38,24 +40,19 @@ void RequestHandler::kick() {
   std::string target;
   while (std::getline(ss, target, ',')) {
     // 해당 유저가 채널에 없을 때
-    if (memberList.find(target) == memberList.end()) {
-      _msg.ErrUserNotInChannel(_fd, target, _token[1]);
+    if (!chanToKick->isMember(target)) {
+      _msg.ErrUserNotInChannel(_fd, target, channelName);
     } else {
-      std::set<std::string>::const_iterator it;
-      for (it = memberList.begin(); it != memberList.end(); ++it) {
-        _msg.setPrefix(_client->getPrefix());
-        _msg.setParam("KICK " + _token[1] + " " + target);
-        std::string trailing = "";
-        for (size_t i = 3; i < _token.size(); ++i) {
-          trailing += _token[i];
-          if (i < _token.size() - 1) {
-            trailing += " ";
-          }
-          _msg.setTrailing(trailing);
-        }
-        _msg.sendToClient(Server::_clientNicks[*it]->getFd());
+      _msg.setPrefix(_client->getPrefix());
+      _msg.setParam("KICK " + channelName + " " + target);
+      std::string trailing;
+      for (size_t i = 3; i < _token.size(); ++i) {
+        trailing += _token[i];
+        if (i < _token.size() - 1) trailing += " ";
+        _msg.setTrailing(trailing);
       }
-      _client->leaveChannel(chanToKick);
+      chanToKick->sendToAll(_msg);
+      Server::_clientNicks[target]->leaveChannel(chanToKick);
     }
   }
 }
